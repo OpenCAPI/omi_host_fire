e525bad6064078ea7f4a0589adbf1c65dfe2502c
Allison Brian D (Brian D) <allison@rchgl5179.rchland.ibm.com>
2020-09-21 16:53:38 -0500

    Removing extra files

--- build/.Xil/Vivado-29006-rchgl6186.rchland.ibm.com/u_ila_rclk_CV.0/out/dcp/u_ila_rclk_stub.v
+++
@@ -1,9 +0,0 @@
-// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
-
-// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
-// Please paste the declaration into a Verilog source file or add the file as an additional source.
-(* X_CORE_INFO = "ila,Vivado 2018.3.1" *)
-module u_ila_rclk(clk, probe0);
-  input clk;
-  input [0:0]probe0;
-endmodule
